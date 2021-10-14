786fb18db3aae2921fbf990acce7ed015a7afcac
Nina Y <nyuchi@risd.edu>
2021-10-14 12:49:33 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+	$("#button").click(function(){
+	var i = Math.floor(Math.random() * (5 - 1) + 1);
+	console.log(i);
+	var imageHold = $("#imageHold");
+	var text = $("#description");
+	switch (i){
+		case 1:
+			text.html("In order to become a human, she had to obtain a body. The pixel travelled to Google, the most vast avenue of the internet where she was bombarded with advertisements, abandoned domains, and similar wandering pixels like her. Faceless, bodiless, devoid of anything besides their 1 x 1px existences. “dress up games for girls,” she urgently searched and clicked the first result that appeared. As if she wasn’t already overwhelmed by the extravagance of the digital world, she sure was now by the seemingly endless amount of blouses and skirts this game offered. She urgently picked the most glittery dress available and the highest heels her avatar could wear. Stepping out of the game with bouncy brunette hair and a heavy eyeliner, she finally had a body!");
+			imageHold.css('backgroundImage', 'url("bride2.jpg")');
+		break;
+		case 2:
+			text.html("Part of becoming human is forming connections. As a pixel, the girl had nobody besides the other pixels pressed next to her. They never talked and never spoke. Unlike her, none of them seemed like they wanted to escape the life of a pixel. However, the girl was different and thought to herself, “I’m going to find other humans to talk to. I’m going to find a human boyfriend.” She slipped into the digital alleyways of online chatrooms, donning the alias of @cyber_sweetheart and messaging as many users as she possibly could.\n\n\n“hey cyber_sweetheart where are u from,” a message from sput.nicholas read. “ur avi is cute lol.”\n\n“thanx!! i’m from florida haha,” she replied. She had only heard about Florida from obscene Youtube videos. “ur probably cute too!”\n\nCountless lines of chat messages later, their conversation shifted to sput.nicholas’ home life and relationship problems, while the Cyber Sweetheart eagerly processed it all. “wow, your gf sounds like she sucks! she doesn’t deserve u :(” she insisted.\n\n“aww yeah i wish i was dating someone like u instead :(“\nIs part of being human falling in love?");
+			imageHold.css('backgroundImage', 'url("bride3.jpg")');
+		break;
+		case 3:
+			text.html("“She first slipped off her cardigan which was hardly on to begin with, revealing a lacy red bra on a pair of perky breasts and matching undies covering her clean shaven pussy. She took slow steps closer before dropping on her knees at his figure, eyes widening as she faced a bulging, enormous, erect c-“ Glancing over her words one last time and taking a deep breath, the girl pressed the “send” button in front of her. A few minutes later, a notification slid into her peripheral: ”User5823 transferred $100 into your account.” Finally, she was a human. She was making connections with so many people through the internet and even more so who appreciated her very human body. She was earning money, which she used to purchase new items for all the avatars she accumulated on dozens of roleplaying sites. With the new $100 in her account she could buy the newly released outfits on imvu, one of which was a skimpy slip dress that would surely impress her imvu girlfriend. A heavy loneliness continued to weigh on the Cyber Sweetheart’s digital body, so she was confused. If she was a human now with countless partners and a growing bank account, why did she feel this way? So, she continued to search the corners of the internet in attempt to become truly human. Perhaps she wasn’t searching in the right places.");
+			imageHold.css('backgroundImage', 'url("bride4.jpg")');
+		break;
+		case 4:
+			text.html("The Cyber Sweetheart tried to cry but she couldn’t utter a single sound as she desperately tried to call out to the user on the other side of the screen. Notice me, look at me, she wanted to say in her suffocating cell of an advertisement. No matter how attractive she tried to look, hardly anybody clicked on her and instead ignored her with an expression of disgust or even worse, apathy. Every so often some poor fellows, overcome with curiosity or simply desperation, would click on her only to regret it. The Cyber Sweetheart began to corrupt and her mind grew scrambled the more she tried to connect with those on the other side of the screen. Her previously manicured nails and permed hair glitched with bugs and viruses until she couldn’t bear to look at her bodies anymore. Slowly, the essence of her existence faded and slowly, she returned to her life as a pixel.");
+			imageHold.css('backgroundImage', 'url("bride5.jpg")');
+		}
+	});
+});
